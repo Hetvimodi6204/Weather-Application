751c6b82a53884f8b2c73619d49b22294a8754f0
Hetvimodi6204 <139551223+Hetvimodi6204@users.noreply.github.com>
2024-03-30 20:25:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+//https://api.openweathermap.org/data/2.5/weather?lat=23.58&lon=72.36&appid=cb777e9b5a4b322ae0b5ff6480a82a72
+//api.openweathermap.org/data/2.5/weather?q=pune&appid=cb777e9b5a4b322ae0b5ff6480a82a72
